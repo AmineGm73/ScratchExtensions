5535e8fdf069d413ec2fd7ae9671818cae3e9744
AmineGm73 <124823874+AmineGm73@users.noreply.github.com>
2023-04-17 18:51:41 +0200

    Update files extension.js

--- files extension.js
+++ files extension.js
@@ -1,65 +1,62 @@
 class FileExtension {
-    constructor(runtime) {
-      this.runtime = runtime;
-      this.files = {};
-    }
-  
-    getInfo() {
-      return {
-        id: 'helloFile',
-        name: 'Hello File',
-        blocks: [
-          {
-            opcode: 'saveValueAsText',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'Save [value] as [filename] file',
-            arguments: {
-              value: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: 'Hello, World!'
-              },
-              filename: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: 'hello.txt'
-              }
-            }
-          },
-          {
-            opcode: 'readValueFromFile',
-            blockType: Scratch.BlockType.REPORTER,
-            text: 'Read value from [filename] file',
-            arguments: {
-              filename: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: 'hello.txt'
-              }
+  getInfo() {
+    return {
+      id: 'file',
+      name: 'File',
+      blocks: [
+        {
+          opcode: 'saveValueAsText',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'Save [value] as [filename] file',
+          arguments: {
+            value: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'Hello, World!'
+            },
+            filename: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'hello.txt'
             }
           }
-        ]
-      };
-    }
-  
-    saveValueAsText(args) {
-      const value = args.value;
-      let filename = args.filename;
-      const extension = filename.split('.').pop();
-  
-      filename += '.txt';
-
-  
-      this.files[filename] = value;
-    }
-  
-    readValueFromFile(args) {
-      const filename = args.filename;
-  
-      if (filename in this.files) {
-        return this.files[filename];
-      } else {
-        return '';
-      }
-    }
+        },
+        {
+          opcode: 'readValueFromFile',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Read value from [filename] file',
+          arguments: {
+            filename: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'hello.txt'
+            }
+          }
+        }
+      ]
+    };
   }
-  
-  Scratch.extensions.register(new FileExtension());
-  
+
+  saveValueAsText(args) {
+    const value = args.value;
+    let filename = args.filename;
+    const extension = filename.split('.').pop();
+
+    // If the file extension is not .txt, .lua, or .py, set it to .txt by default
+    if (extension !== 'txt' && extension !== 'lua' && extension !== 'py') {
+      filename += '.txt';
+    }
+
+    // Add your custom logic here to save the value as text with the specified filename
+    console.log('Saving value:', value, 'to file:', filename);
+  }
+
+  readValueFromFile(args) {
+    const filename = args.filename;
+
+    // Add your custom logic here to read the value from the specified filename
+    console.log('Reading value from file:', filename);
+
+    // Return the read value from the file, or an empty string if the file does not exist
+    return '';
+  }
+}
+
+Scratch.extensions.register(new FileExtension());
