4d0ea09148e48670b39e5626d8ed4bc4bd80671b
AmineGm73 <124823874+AmineGm73@users.noreply.github.com>
2023-05-08 19:33:43 +0200

    Maths_Extension.js

--- Maths_Extension.js
+++ Maths_Extension.js
@@ -1,26 +1,22 @@
 (function (ext) {
-    // Extension code will be inserted here
-  
-    // Arctangent function (atan2)
-    ext.atan2 = function (y, x) {
-      return Math.atan2(y, x) * (180 / Math.PI);
-    };
-  
-    // Linear interpolation function (lerp)
-    ext.lerp = function (start, end, t) {
-      return start + t * (end - start);
-    };
-  
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = function () {};
-  
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function () {
-      return { status: 2, msg: 'Ready' };
-    };
-  
-    // Register the extension
-    ScratchExtensions.register('Math Extension', ext, { type: 'misc' });
-  })({});
-  
+  // Load external JavaScript file
+  var extensionUrl = 'https://aminegm73.github.io/ScratchExtensions/Maths_Extension.js';
+  var script = document.createElement('script');
+  script.src = extensionUrl;
+  document.head.appendChild(script);
+
+  // Cleanup function when the extension is unloaded
+  ext._shutdown = function () {
+    // Remove the dynamically added script element
+    document.head.removeChild(script);
+  };
+
+  // Status reporting code
+  // Use this to report missing hardware, plugin or unsupported browser
+  ext._getStatus = function () {
+    return { status: 2, msg: 'Ready' };
+  };
+
+  // Register the extension
+  ScratchExtensions.register('Math Extension', ext, { type: 'misc' });
+})({});
